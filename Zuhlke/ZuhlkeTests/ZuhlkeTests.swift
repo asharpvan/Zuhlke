8d222f41b70d604fa5a3aa258bf501c91206198d
Pranav Sah <iospranav1712@gmail.com>
2020-08-21 18:23:08 +0800

    Started Project

---
+++ Zuhlke/ZuhlkeTests/ZuhlkeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZuhlkeTests.swift
+//  ZuhlkeTests
+//
+//  Created by #HellRaiser on 21/08/20.
+//  Copyright © 2020 asharpvan. All rights reserved.
+//
+
+import XCTest
+@testable import Zuhlke
+
+class ZuhlkeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
